cb49c385f46ece22ea550f07811c16324475fa63
ClemBesacier <clementinebesacier@yahoo.fr>
2019-10-10 15:32:33 +0200

    My first commit symphony

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+class DefaultController
+{
+    public function homepage()
+    {
+        return new Response('Hello World !');
+    }
+}
